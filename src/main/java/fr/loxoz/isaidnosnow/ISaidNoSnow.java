e9730e383bb82deaa0e34eb93a7c039cbd26dcd3
Loxoz <37414842+Loxoz@users.noreply.github.com>
2022-07-30 22:40:08 +0200

    1.0.1 - 1.19
    Now supports saving the last state

--- src/main/java/fr/loxoz/isaidnosnow/ISaidNoSnow.java
+++ src/main/java/fr/loxoz/isaidnosnow/ISaidNoSnow.java
@@ -3,18 +3,17 @@ package fr.loxoz.isaidnosnow;
 import net.fabricmc.api.ClientModInitializer;
 import net.fabricmc.api.EnvType;
 import net.fabricmc.api.Environment;
+import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientLifecycleEvents;
 import net.fabricmc.fabric.api.client.event.lifecycle.v1.ClientTickEvents;
 import net.fabricmc.fabric.api.client.keybinding.v1.KeyBindingHelper;
-import net.minecraft.block.BlockState;
-import net.minecraft.block.Blocks;
+import net.fabricmc.loader.api.FabricLoader;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.option.KeyBinding;
 import net.minecraft.client.util.InputUtil;
-import net.minecraft.network.MessageType;
-import net.minecraft.text.TranslatableText;
-import net.minecraft.util.Util;
-import org.lwjgl.glfw.GLFW;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
+import net.minecraft.text.Text;
+
+import java.io.*;
+import java.util.Properties;
 
 @Environment(EnvType.CLIENT)
 public class ISaidNoSnow implements ClientModInitializer {
@@ -32,6 +31,34 @@ public class ISaidNoSnow implements ClientModInitializer {
         keyToggle = KeyBindingHelper.registerKeyBinding(new KeyBinding("key." + MOD_ID + ".toggle", InputUtil.Type.KEYSYM, -1, "key.categories." + MOD_ID));
 
         ClientTickEvents.END_CLIENT_TICK.register(this::onTick);
+        ClientLifecycleEvents.CLIENT_STOPPING.register(this::saveConfig);
+        loadConfig();
+    }
+
+    public File getConfigFile() {
+        return new File(FabricLoader.getInstance().getConfigDir().toFile(), MOD_ID + ".properties");
+    }
+
+    public void loadConfig() {
+        var file = getConfigFile();
+        if (!file.exists() || !file.isFile()) return;
+        try {
+            var reader = new FileReader(file);
+            var props = new Properties();
+            props.load(reader);
+            String res = props.getProperty("enabled");
+            if (res == null) return;
+            enabled = Boolean.parseBoolean(res);
+        } catch (IOException ignored) {}
+    }
+
+    public void saveConfig(MinecraftClient client) {
+        try {
+            var reader = new FileWriter(getConfigFile());
+            var props = new Properties();
+            props.setProperty("enabled", String.valueOf(enabled));
+            props.store(reader, null);
+        } catch (IOException ignored) {}
     }
 
     public void onTick(MinecraftClient client) {
@@ -47,7 +74,7 @@ public class ISaidNoSnow implements ClientModInitializer {
         if (this.enabled != enabled) {
             this.enabled = enabled;
             getClient().worldRenderer.reload();
-            getClient().inGameHud.addChatMessage(MessageType.GAME_INFO, new TranslatableText("isaidnosnow.text.snow_" + (this.enabled ? "hide" : "show")), Util.NIL_UUID);
+            getClient().inGameHud.setOverlayMessage(Text.translatable("text.isaidnosnow.snow_" + (this.enabled ? "hide" : "show")), false);
         }
     }
 }
